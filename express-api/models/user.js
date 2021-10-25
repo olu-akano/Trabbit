38282a89de4abdd3732f5f7a68ac708003756dd7
glnkvn <golnarkavianii@gmail.com>
2021-10-25 10:33:27 +0100

    add get all funtion for users

---
+++ express-api/models/user.js
@@ -0,0 +1,27 @@
+const { init } = require ('../db_config/config')
+
+class User {
+    constructor(data){
+        this.id = data.id
+        this.name = data.name
+        this.habit = data.habit
+        //add any other variables a user has later
+    }
+
+    static get all() {
+        return new Promise (async (resolve, reject) => {
+            try {
+                const db = await init()
+                const userData = await db.collection('users').find().toArray()
+                const users = userData.map(d => new Entry({ ...d, id: d._id }))
+                resolve(users);
+            } catch (err) {
+                console.log(err);
+                reject("Error retrieving users")
+            }
+        })
+    }
+
+}
+
+module.exports = User;
