6ada66744d5c9f1f3ae4e03793a55586ec9a87ef
olu-akano <akanopaul@yahoo.co.uk>
2021-10-25 14:31:08 +0100

    setup auth and auth

---
+++ express-api/controllers/auth.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+
+const express = require('express');
+const router = express.Router();
+
+const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+
+const User = require('../models/user');
+
+router.post('/register', async (req, res) => {
+    try {
+        const salt = await bcrypt.genSalt();
+        const hashed = await bcrypt.hash(req.body.password, salt)
+        await User.create({...req.body, password: hashed})
+        res.status(201).json({msg: 'User created'})
+    } catch (err) {
+        res.status(500).json({err});
+    }
+})
+
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findByEmail(req.body.email)
+        if(!user){ throw new Error('There is no user with this email') }
+        const authed = bcrypt.compare(req.body.password, user.passwordDigest)
+        if (!!authed){
+            const payload = { username: user.username, email: user.email }
+            const sendToken = (err, token) => {
+                if(err){ throw new Error('Error in token generation') }
+                res.status(200).json({
+                    success: true,
+                    token: "Bearer " + token,
+                });
+            }
+            jwt.sign(payload, process.env.SECRET, { expiresIn: 60 }, sendToken);
+        } else {
+            throw new Error('User could not be authenticated')  
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(401).json({ err });
+    }
+})
