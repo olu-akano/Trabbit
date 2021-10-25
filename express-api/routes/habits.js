15e0b8dc732cc3c9f02c1110f1dd3fc1f29fa321
olu-akano <akanopaul@yahoo.co.uk>
2021-10-26 00:05:23 +0100

    add json web tokens to file structure

--- express-api/routes/habits.js
+++ express-api/routes/habits.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const habitsController = require('../controllers/habits')
 
+const {verifyToken} = require('../middleware/auth');
 
-router.get('/', habitsController.index);
-
-router.get('/:id',habitsController.show);
-router.post('/',habitsController.create);
-router.patch('/:id',habitsController.update);
-router.delete('/:id',habitsController.destroy)
+router.get('/', verifyToken, habitsController.index);
+router.get('/:id', verifyToken, habitsController.show);
+router.post('/', verifyToken, habitsController.create);
+router.patch('/:id', verifyToken, habitsController.update);
+router.delete('/:id', verifyToken, habitsController.destroy)
 
 module.exports = router;
