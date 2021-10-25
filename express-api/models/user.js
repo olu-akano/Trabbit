89db8bb9969bb64f6fc558743c507e9c13e6457d
glnkvn <golnarkavianii@gmail.com>
2021-10-25 10:48:58 +0100

    add get all funtion for habits

--- express-api/models/user.js
+++ express-api/models/user.js
@@ -2,22 +2,22 @@ const { init } = require ('../db_config/config')
 
 class User {
     constructor(data){
-        this.id = data.id
-        this.name = data.name
-        this.habit = data.habit
+        this.id = data.id;
+        this.name = data.name;
+        this.habit = data.habit;
         //add any other variables a user has later
     }
 
     static get all() {
         return new Promise (async (resolve, reject) => {
             try {
-                const db = await init()
-                const userData = await db.collection('users').find().toArray()
-                const users = userData.map(d => new Entry({ ...d, id: d._id }))
+                const db = await init();
+                const userData = await db.collection('users').find().toArray();
+                const users = userData.map(d => new User({ ...d, id: d._id }));
                 resolve(users);
             } catch (err) {
                 console.log(err);
-                reject("Error retrieving users")
+                reject("Error retrieving users");
             }
         })
     }
