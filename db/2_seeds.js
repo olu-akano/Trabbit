3753b92280cb43d62d566cd62cf929a54275b18e
olu-akano <akanopaul@yahoo.co.uk>
2021-10-25 11:16:33 +0100

    setup database structure

---
+++ db/2_seeds.js
@@ -0,0 +1,13 @@
+db = connect("localhost:27017/")
+db.users.drop()
+db.habits.drop()
+
+db.users.insertMany([
+    {username: "Bob", email: "bob@yeah.com", password_digest: "bluesky"},
+    {username: "Skylar", email: "skylar@yeah.com", password_digest: "rednight"}
+])
+
+db.habits.insertMany ([
+    {habitname: "exercising", frequency: 5, current_count: 10, streak: 2 },
+    {habitname: "reading", frequency: 3, current_count: 3, streak: 1 }
+])
