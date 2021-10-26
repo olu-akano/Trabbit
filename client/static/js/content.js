5e23129406883ab48d26827480b5f68054b657d9
glnkvn <golnarkavianii@gmail.com>
2021-10-26 15:28:04 +0100

    add getPostById function

--- client/static/js/content.js
+++ client/static/js/content.js
@@ -1,5 +1,7 @@
+const classOverview=document.getElementById("userpage-habits");
 const checkIds=[];
-let count=2;
+const checkDatas=[];
+let count=0;
 
 async function renderHabits(){
     try {
@@ -19,20 +21,20 @@ async function renderHabits(){
 
 async function render(data){
 
-    count++;
-    
-
     const table=document.getElementById('habits-textcontent');
     const tableRow=document.createElement('tr');
     tableRow.className="habit";
     const tableData_1=document.createElement('td');
     tableData_1.textContent=`\u2022`;
+    tableData_1.style.width='10%';
     const tableData_2=document.createElement('td');
     tableData_2.textContent=data.habitname;
+    tableData_2.style.width='50%';
     const tableData_3=document.createElement('td');
     tableData_3.textContent='More info...';
     tableData_3.className='more-info-btn';
     tableData_3.id='more-info-btn-'+count;
+    tableData_3.style.width='20%';
     checkIds.push(tableData_3.id);
     const tableData_4=document.createElement('td');
     const emojy="🔥";
@@ -44,22 +46,21 @@ async function render(data){
     tableRow.append(tableData_4);
 
     table.append(tableRow);
+    checkDatas.push(data);
+    console.log(`checkData: ${checkDatas[0].streak}`);
     console.log(`checkId: ${checkIds}`);
-    setID(checkIds)
 
-    
+    setID(checkIds[count],checkDatas[count])
+    count++;
 
-
-
-    function setID(checkIds){
-        for (let i =0 ; checkIds.length > i;  i++) {
-            const getIdNum=document.getElementById(checkIds[i]);
+    function setID(checkIds,checkDatas){
+        // for (let i =0 ; checkIds.length > i;  i++) {
+            const getIdNum=document.getElementById(checkIds);
             getIdNum.addEventListener('click', (e) => {
-                console.log();
-                getIdNum.style.backgroundColor="red";
-                // getPostById(checkIds[i],selectPostID[i]);
+                classOverview.className="hideClass";
+                getPostById(checkDatas);
             })
-        }
+        // }
     }
 
     
@@ -67,20 +68,27 @@ async function render(data){
 
 renderHabits();
 
-async function zoomIn(data){
+async function getPostById(data){
+    console.log(data.habitname);
+    console.log('working');
+    const div=document.getElementById('activity');
+    const newHabitName=document.createElement('h2');
+    const newCount=document.createElement('h2');
+    const newFrequency=document.createElement('h2');
+    const newStrak=document.createElement('h2');
+
+    div.textContent='activity information';
+    newHabitName.textContent=data.habitname;
+    newCount.textContent=data.current_count;
+    newFrequency.textContent=data.frequency;
+    newStrak.textContent=data.streak;
+
+    div.append(newHabitName);
+    div.append(newCount);
+    div.append(newFrequency);
+    div.append(newStrak);
 
 }
-
-// for(var i=0; i < count; i++){
-
-//     text.push(`more-info-btn-${i+1}`);
-//     textSelect.push(document.getElementById(`${text[i]}`));
-//     console.log(text[i]);
-//     textSelect[i].addEventListener('click', (e)=> {
-//         textSelect[i].style.backgroundColor="red";
-//     })
-// }
-
 
 
 // When page is loaded, render habits
