94529a7dff1e139e627c4796640bbd04c8196d5f
olu-akano <akanopaul@yahoo.co.uk>
2021-10-26 16:23:25 +0100

    add test for habits controller index

---
+++ express-api/tests/unit/controllers/habits.test.js
@@ -0,0 +1,24 @@
+const habitsController = require('../../../controllers/habits')
+const Habit = require('../../../models/habits')
+
+const mockSend = jest.fn();
+const mockJson = jest.fn();
+const mockStatus = jest.fn(code => ({ send: mockSend, json: mockJson, end: jest.fn() }));
+const mockRes = { status: mockStatus};
+
+describe ('habits controller', () => {
+    beforeEach(() => jest.clearAllMocks());
+
+    afterAll(() => jest.resetAllMocks ());
+
+    describe('index', () => {
+        test('it returns habits with a 200 status code', async () => {
+            let testHabits = ['habit1', 'habit2']
+            jest.spyOn(Habit, 'all', 'get')
+                 .mockResolvedValue(testHabits);
+            await habitsController.index(null, mockRes);
+            expect(mockStatus).toHaveBeenCalledWith(200);
+            expect(mockJson).toHaveBeenCalledWith(testHabits);
+        })
+    }); 
+})
