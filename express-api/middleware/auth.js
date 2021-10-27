e4fc123d47f4fca02a3cde4fa90113084435687e
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-10-27 17:02:08 +0100

    fiddling with hosted services

--- express-api/middleware/auth.js
+++ express-api/middleware/auth.js
@@ -4,7 +4,6 @@ function verifyToken(req, res, next){
     const header = req.headers['authorization'];
     if (header) {
         const token = header.split(' ')[1];
-        console.log(token);
         jwt.verify(token, process.env.SECRET, async (err, data) => {
             console.log(data);
             if(err){
--- express-api/routes/habits.js
+++ express-api/routes/habits.js
@@ -4,17 +4,18 @@ const habitsController = require('../controllers/habits')
 
 const {verifyToken} = require('../middleware/auth');
 
-router.get('/', habitsController.index);
-router.get('/:id', habitsController.show);
-router.get('/users/:username', habitsController.showUsers);
-router.post('/', habitsController.create);
-router.patch('/:id', habitsController.update);
-router.delete('/:id', habitsController.destroy)
 // router.get('/', verifyToken, habitsController.index);
 // router.get('/:id', verifyToken, habitsController.show);
 // router.get('/users/:username', verifyToken, habitsController.showUsers);
 // router.post('/', verifyToken, habitsController.create);
 // router.patch('/:id', verifyToken, habitsController.update);
 // router.delete('/:id', verifyToken, habitsController.destroy)
+
+router.get('/', habitsController.index);
+router.get('/:id', habitsController.show);
+router.get('/users/:username', habitsController.showUsers);
+router.post('/', habitsController.create);
+router.patch('/:id', habitsController.update);
+router.delete('/:id', habitsController.destroy)
 
 module.exports = router;
