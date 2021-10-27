78a52697401de23b75675d4982db8b8b3d5816f8
Robert Bristow <robertbristow3@gmail.com>
2021-10-27 10:23:05 +0100

    allow 'hobby' category to be properly added with a new habit

--- client/static/js/requests.js
+++ client/static/js/requests.js
@@ -37,7 +37,7 @@ async function addNewHabit(e) {
         
         //for loop to get the right habit form
         let habit = "";
-        for(i = 1; i < form.length - 4; i++) {            
+        for(i = 1; i < form.length - 3; i++) {            
             if(!!e.target[i].value) {
                 habit = e.target[i].value
             }
@@ -48,7 +48,7 @@ async function addNewHabit(e) {
             frequency: e.target[4].value,
             currentcount: 0,
             streak: 0,
-            // description: e.target[5].value ---Commented out for now
+            description: e.target[5].value
         };
         console.log(newHabit);
         const options = {
