6dca37d8f1752c83f3ab1271236f3e91dd14a570
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-10-27 17:18:20 +0100

    return to initial state

--- express-api/db_config/config.js
+++ express-api/db_config/config.js
@@ -1,7 +1,6 @@
 const { MongoClient } = require('mongodb')
 const connectionUrl = process.env.DB_CONNECTION;
 const dbName = process.env.DB_NAME
-// const connectionUrl = `mongodb+srv://trabbit:corgie@cluster0.wwvgu.mongodb.net/${dbName}?retryWrites=true&w=majority`
 
 const init = async () => {
   let client = await MongoClient.connect(connectionUrl)
