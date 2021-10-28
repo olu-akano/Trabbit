1c0a147b2a911f3e8fc34275761f51497e4e5e27
glnkvn <golnarkavianii@gmail.com>
2021-10-28 14:45:13 +0100

    Update tests

--- express-api/tests/unit/models/Habits.test.js
+++ express-api/tests/unit/models/Habits.test.js
@@ -22,7 +22,7 @@ describe('Author', () => {
         test('it resolves with message on successful db query', async () => {
             jest.spyOn(db, 'query')
                 .mockResolvedValueOnce({ id: 1 });
-            let testHabit = new Author({ id: 1, habitname: 'test habit', frequency: 5, current_count: 5, streak: 1})
+            let testHabit = new Habit({ id: 1, username: 'Test' , habitname: 'test habit', description: 'Testing', frequency: 5, current_count: 5, streak: 1})
             const result = await testHabit.destroy();
             expect(result).toBe('Habit 1 was deleted')
         })
@@ -30,7 +30,7 @@ describe('Author', () => {
 
     describe('create', () => {
         test('it resolves with an habit on successful db query', async () => {
-            let habitData = { id: 1, habitname: 'test habit', frequency: 5, current_count: 5, streak: 1 }
+            let habitData = { id: 1, username: 'Test' ,habitname: 'test habit', description: 'Testing', frequency: 5, current_count: 5, streak: 1 }
             jest.spyOn(db, 'query')
                 .mockResolvedValueOnce({rows: [ habitData] });
             const result = await Habit.create('New Habit');
@@ -40,7 +40,7 @@ describe('Author', () => {
 
     describe('findById', () => {
         test('it resolves with habit on successful db query', async () => {
-            let authorData = {id: 1, habitname: 'test habit', frequency: 5, current_count: 5, streak: 1 }
+            let authorData = {id: 1, username: 'Test',habitname: 'test habit',description: 'Testing', frequency: 5, current_count: 5, streak: 1 }
             jest.spyOn(db, 'query')
                 .mockResolvedValueOnce({rows: [ authorData] });
             const result = await Author.findById(1);
