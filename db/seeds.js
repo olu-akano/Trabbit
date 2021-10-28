efe2bf47f2a76af6f5a5e8d7740c53d38fb7e9e3
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-10-28 10:25:33 +0100

    add description to one of hte seeded habits

--- db/seeds.js
+++ db/seeds.js
@@ -11,5 +11,5 @@ db.users.insertMany([
 db.habits.insertMany ([
     {username: "Bob", habitname: "exercising", description: "Go for a morning jog", frequency: 5, current_count: 4, streak: 2 },
     {username: "Bob", habitname: "reading", description: "Read a book before bed", frequency: 3, current_count: 3, streak: 1 },
-    {username: "Skylar", habitname: "get 5-a-day", description: "", frequency: 7, current_count: 3, streak: 1 }
+    {username: "Skylar", habitname: "get 5-a-day", description: "Eat 5 portions of fruit/veg a day", frequency: 7, current_count: 3, streak: 1 }
 ])
